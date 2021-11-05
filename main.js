32a60b93a15abf9ebacdfefbb7e617db14500884
Umarjon <sharofiddinov.07@mail.ru>
2021-11-05 20:53:40 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api")
 
-const TOKEN = `2094815044:AAFoVVcRjdsqukbocxJCwTcEU0FADbLqXyA`
+const TOKEN = `2048632800:AAGCgjdxflFaSeheBmfts-7sSs44tUI8R78`
 const bot = new TelegramBot(TOKEN, {
     polling: true,
     updates: {
