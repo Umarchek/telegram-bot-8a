b235fbab646108574fd03ed913d752368c848ecf
UZBase <sharofiddinov.07@mail.ru>
2021-11-09 14:31:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "2094815044:AAFoVVcRjdsqukbocxJCwTcEU0FADbLqXyA";
+const TOKEN = "2048632800:AAGCgjdxflFaSeheBmfts-7sSs44tUI8R78";
 const bot = new TelegramBot(TOKEN, {
   polling: true,
   updates: {
